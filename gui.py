3e280c40938bec1477e156a7f944e8e37420a30f
EOF <1stgfaithh@gmail.com>
2025-01-28 17:09:46 +0100

    Initial Commit

--- gui.py
+++ gui.py
@@ -5,6 +5,7 @@ label = sg.Text("Type in a to-do")
 inputBox = sg.InputText(tooltip="Enter todo")
 addButton = sg.Button("Add")
 
-window = sg.Window("Welcome to my To-Do App", layout=[[inputBox, addButton], [label]])
+window = sg.Window("Welcome to my To-Do App",
+                   layout=[[inputBox, addButton], [label]])
 window.read()
 window.close()
